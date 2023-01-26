71123ea8539e4ba73d1aeb00a14449ef7ae036a2
ArthurAthayde <arthur.athaydee@gmail.com>
2023-01-25 22:03:12 -0300

    css e feat js att

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,37 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+
+function renderModal() {
+    const button = document.querySelector('.openModalBtn');
+    const modal = document.querySelector('.modal__container');
+
+    button.addEventListener('click', () => {
+        modal.showModal();
+
+        closeModal()
+    })
+}
+
+function renderModalSpan() {
+    const span = document.getElementById('openModalSpan');
+    const modal = document.querySelector('.modal__container');
+
+
+    span.addEventListener('click', () => {
+        console.log('oi');
+        modal.showModal();
+
+        closeModal()
+    })
+}
+
+function closeModal() {
+    const closeBtn = document.querySelector('.closeBtn');
+    const modal = document.querySelector('.modal__container');
+
+    closeBtn.addEventListener('click', () => {
+        modal.close()
+    })
+}
+
+renderModal()
+renderModalSpan()
